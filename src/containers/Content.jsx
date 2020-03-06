299ddc23f76caa2c040758959e8bc896eae57db4
jdromero88 <jdromerov88@gmail.com>
2020-03-06 12:08:45 -0500

    Added bootstrap

---
+++ src/containers/Content.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+const Content = () => {
+  return(
+    <>
+      <h1>Hola Im using bootstrap</h1>
+    </>
+  )
+}
+export default Content
---
+++ src/containers/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+const Footer = () => {
+  return(
+    <>
+      <footer>
+        <p>
+          El footer va aqui
+        </p>
+      </footer>
+    </>
+  )
+}
+export default Footer
