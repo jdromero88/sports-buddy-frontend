c0c1334bad1f7ae7c99a0a97d0c0d5302092fc43
jdromero88 <jdromerov88@gmail.com>
2020-05-05 17:54:54 -0400

    Added User view

--- src/containers/Content.jsx
+++ src/containers/Content.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Route, Switch, Redirect } from 'react-router-dom'
 import Home from '../components/Home'
 import Sports from './Sports'
+import Users from './Users'
 const Content = () => {
   return(
     <>
       <Switch>
         <Route exact path='/' component={Home} />
         <Route exact path='/sports' component={Sports} />
+        <Route exact path='/users' component={Users} />
       </Switch>
     </>
   )
---
+++ src/containers/Users.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import UserCard from '../components/UserCard'
+const Users = () => {
+  return (
+    <UserCard />
+  )
+}
+
+export default Users
